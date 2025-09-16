efe6e447302a6463f94aca8ee0acf52bd5cf58b5
Henry Ing-Simmons <hing-simmons@aquilalearning.com>
2025-09-16 15:58:25 +0100

    Add weather API

---
+++ AquilaTodos.Tests/Weather/WeatherControllerTests.cs
@@ -0,0 +1,31 @@
+﻿namespace AquilaTodos.Tests.Weather;
+
+using AquilaTodos.Weather;
+using Shouldly;
+
+public class WeatherApplicationFactory : TestApplicationFactory
+{
+    protected override void SeedData(TodoContext context)
+    {
+        // No seeding required for weather tests
+    }
+}
+
+public class WeatherControllerTests : IClassFixture<WeatherApplicationFactory>
+{
+    private readonly WeatherApplicationFactory factory;
+
+    public WeatherControllerTests(WeatherApplicationFactory factory)
+    {
+        this.factory = factory;
+    }
+
+    [Fact]
+    public async Task Should_return_weather_for_city()
+    {
+        using var client = this.factory.CreateClient();
+        var result = await client.GetObjectAsync<WeatherDto>("api/weather/London");
+
+        result.ShouldNotBeNull();
+    }
+}
---
+++ AquilaTodos/Weather/WeatherDto.cs
@@ -0,0 +1,8 @@
+﻿namespace AquilaTodos.Weather;
+
+public record WeatherDto
+{
+    public required string Temperature { get; init; }
+    public required string Wind { get; init; }
+    public required string Description { get; init; }
+}
