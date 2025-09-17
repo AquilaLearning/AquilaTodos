611aeebe2b2c81252ccf47f1f9e033408b287ac1
Henry Ing-Simmons <hing-simmons@aquilalearning.com>
2025-09-17 15:59:08 +0100

    Update searchItems.spec.ts

--- app/src/__tests__/searchItems.spec.ts
+++ app/src/__tests__/searchItems.spec.ts
@@ -2,25 +2,30 @@ import { searchItems } from "@/todoLists/items/searchItems";
 import type { TodoItemDto } from "@/todoLists/items/TodoItemDto";
 import { describe, expect, it } from "vitest";
 
-const getTestTodoItems = (): TodoItemDto[] => {
-    return [
-            {
-                id: 1,
-                label: "Buy eggs",
-                complete: false
-            },
-            {
-                id: 2,
-                label: "Do laundry",
-                complete: false
-            },
-            {
-                id: 3,
-                label: "Go for a run",
-                complete: false
-            }
-        ];
-}
+const items:TodoItemDto[] = [
+    {
+        id: 1,
+        label: "Buy eggs",
+        complete: false
+    },
+    {
+        id: 2,
+        label: "Do laundry",
+        complete: false
+    },
+    {
+        id: 3,
+        label: "Go for a run",
+        complete: false
+    }
+];
 
 describe("searchItems", () => {
+
+    it("should...", () => {
+
+        const search = "";
+        const result = searchItems(items, search);
+        expect(result).toBeTruthy();
+    });
 });
